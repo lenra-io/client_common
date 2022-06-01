e034f01e05727abd325981a76bb3ef21b311ca2c
Jonas Martinez <36544012+jonas-martinez@users.noreply.github.com>
2022-06-01 14:04:09 +0200

    feat: Handle navigation (#8)

--- lib/navigator/common_navigator.dart
+++ lib/navigator/common_navigator.dart
@@ -1,3 +1,18 @@
+import 'package:client_common/navigator/guard.dart';
+import 'package:client_common/navigator/page_guard.dart';
+import 'package:client_common/views/cgu/cgu_page.dart';
+import 'package:client_common/views/cgu/cgu_page_fr.dart';
+import 'package:client_common/views/login/login_page.dart';
+import 'package:client_common/views/profile/change_lost_password_page.dart';
+import 'package:client_common/views/profile/change_password_confirmation_page.dart';
+import 'package:client_common/views/profile/profile_page.dart';
+import 'package:client_common/views/profile/recovery_page.dart';
+import 'package:client_common/views/register/register_page.dart';
+import 'package:client_common/views/verify_code/verifiying_code_page.dart';
+import 'package:flutter/widgets.dart';
+
+typedef CustomPageBuilder = Widget Function(Map<String, String>);
+
 class CommonNavigator {
   static const String homeRoute = "/";
   static const String loginRoute = "/login";
@@ -9,4 +24,50 @@ class CommonNavigator {
   static const String userValidationRoute = "/validation-user";
   static const String cguRoute = "/cgu";
   static const String cguRouteFR = "/cgu/fr";
+  static const String profileRoute = "/profile";
+
+  static String? currentRoute;
+
+  static final Map<String, CustomPageBuilder> authRoutes = {
+    changeLostPasswordRoute: (Map<String, String> params) => PageGuard(
+          guards: [Guard.checkUnauthenticated],
+          child: ChangeLostPasswordPage(),
+        ),
+    lostPasswordRoute: (Map<String, String> params) => PageGuard(
+          guards: [Guard.checkUnauthenticated],
+          child: RecoveryPage(),
+        ),
+    changePasswordConfirmationRoute: (Map<String, String> params) => PageGuard(
+          guards: [Guard.checkUnauthenticated],
+          child: ChangePasswordConfirmationPage(),
+        ),
+    profileRoute: (Map<String, String> params) => PageGuard(
+          guards: [Guard.checkAuthenticated],
+          child: ProfilePage(),
+        ),
+    loginRoute: (Map<String, String> params) => PageGuard(
+          guards: [Guard.checkUnauthenticated],
+          child: LoginPage(),
+        ),
+    registerRoute: (Map<String, String> params) => PageGuard(
+          guards: [Guard.checkUnauthenticated],
+          child: RegisterPage(),
+        ),
+    cguRoute: (Map<String, String> params) => PageGuard(
+          guards: [Guard.checkAuthenticated],
+          child: CguPage(),
+        ),
+    cguRouteFR: (Map<String, String> params) => PageGuard(
+          guards: [Guard.checkAuthenticated],
+          child: CguPageFr(),
+        ),
+    userValidationRoute: (Map<String, String> params) => PageGuard(
+          guards: [
+            Guard.checkAuthenticated,
+            Guard.checkCguAccepted,
+            Guard.checkIsNotUser,
+          ],
+          child: VerifyingCodePage(),
+        ),
+  };
 }
