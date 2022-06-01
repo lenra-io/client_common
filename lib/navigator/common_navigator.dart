f4bebafbfa0df2504d7fc1dd04415c83ba04e9a1
Jonas Martinez <36544012+jonas-martinez@users.noreply.github.com>
2022-06-01 13:54:42 +0200

    feat: Add views (#11)

--- lib/navigator/common_navigator.dart
+++ lib/navigator/common_navigator.dart
@@ -7,4 +7,6 @@ class CommonNavigator {
   static const String changePasswordConfirmationRoute = "/change-confirmation";
   static const String validationDevRoute = "/validation-dev";
   static const String userValidationRoute = "/validation-user";
+  static const String cguRoute = "/cgu";
+  static const String cguRouteFR = "/cgu/fr";
 }
