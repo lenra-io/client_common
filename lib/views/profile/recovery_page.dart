1a5e60b76812ffcf15a8da84dd6bdb795f03869e
Jonas Martinez <36544012+jonas-martinez@users.noreply.github.com>
2022-06-01 13:59:16 +0200

    feat: Add missing views (#12)

---
+++ lib/views/profile/recovery_page.dart
@@ -0,0 +1,19 @@
+import 'package:client_common/navigator/common_navigator.dart';
+import 'package:client_common/views/login/recovery_form.dart';
+import 'package:client_common/views/simple_page.dart';
+import 'package:flutter/material.dart';
+
+class RecoveryPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return SimplePage(
+      title:
+          "Did you lose your password ? Don't worry, it happens even to the best!",
+      backInkText: "Back to the login page",
+      backInkAction: () {
+        Navigator.pushNamed(context, CommonNavigator.homeRoute);
+      },
+      child: RecoveryForm(),
+    );
+  }
+}
