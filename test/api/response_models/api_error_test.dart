7d27a9ab8f6b4f2db9bed8f5d7bd9e9c06218c0f
jonas-martinez <cbobbxta.fr@gmail.com>
2022-07-22 16:25:27 +0200

    fix test

--- test/api/response_models/api_error_test.dart
+++ test/api/response_models/api_error_test.dart
@@ -3,7 +3,7 @@ import 'package:test/test.dart';
 
 void main() {
   test('from json', () {
-    Map<String, dynamic> json = {"reason": "error", "message": "Message d'erreur"};
+    Map<String, dynamic> json = {"reason": "error", "error": "Message d'erreur"};
     ApiError error = ApiError.fromJson(json);
     expect(error.reason, "error");
     expect(error.message, "Message d'erreur");
