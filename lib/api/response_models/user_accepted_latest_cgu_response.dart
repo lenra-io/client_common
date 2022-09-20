308b88ee57eabff4e2302e5725013b794a3eeb25
jonas-martinez <cbobbxta.fr@gmail.com>
2022-09-20 16:34:26 +0200

    fix: Accepted latest cgu receiving Map<String, dynamic> instead of bool

--- lib/api/response_models/user_accepted_latest_cgu_response.dart
+++ lib/api/response_models/user_accepted_latest_cgu_response.dart
@@ -3,5 +3,5 @@ import 'package:client_common/api/response_models/api_response.dart';
 class UserAcceptedLatestCguResponse extends ApiResponse {
   bool accepted;
 
-  UserAcceptedLatestCguResponse.fromJson(bool json) : accepted = json;
+  UserAcceptedLatestCguResponse.fromJson(Map<String, dynamic> json) : accepted = json["accepted"];
 }
