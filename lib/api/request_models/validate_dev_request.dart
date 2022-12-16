c4f67ab775424eb5af81a39fde0f5121bc0c6139
Jonas Martinez <36544012+jonas-martinez@users.noreply.github.com>
2022-12-16 16:29:33 +0100

    feat: Remove dev token (#60)

--- lib/api/request_models/validate_dev_request.dart
+++ lib/api/request_models/validate_dev_request.dart
@@ -1,11 +1,5 @@
 import 'package:client_common/api/request_models/api_request.dart';
 
 class ValidateDevRequest extends ApiRequest {
-  final String code;
-
-  ValidateDevRequest(this.code);
-
-  dynamic toJson() => {
-        "code": code,
-      };
+  dynamic toJson() => {};
 }
