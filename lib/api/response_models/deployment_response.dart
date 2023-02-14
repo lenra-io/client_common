d85d91d5c647739a6f7ac9a2b499624ae16647fd
Emric Pichonnier <emric.pichonnier@lenra.io>
2023-02-14 10:42:24 +0100

    feat: add deployment model (#76)

---
+++ lib/api/response_models/deployment_response.dart
@@ -0,0 +1,33 @@
+import 'package:client_common/api/response_models/api_response.dart';
+
+enum DeploymentStatus { created, waitingForBuild, waitingForAppReady, failure, success }
+
+class DeploymentResponse extends ApiResponse {
+  int id;
+  int buildId;
+  int applicationId;
+  DeploymentStatus status;
+  DateTime insertedAt;
+
+  DeploymentResponse.fromJson(Map<String, dynamic> json)
+      : id = json["id"],
+        status = DeploymentStatus.values.firstWhere((e) => e.toString() == 'DeploymentStatus.${json["status"]}',
+            orElse: () => DeploymentStatus.failure),
+        insertedAt = DateTime.parse(json["inserted_at"]),
+        buildId = json["build_id"],
+        applicationId = json["application_id"];
+
+  @override
+  bool operator ==(Object other) {
+    return other is DeploymentResponse &&
+        other.buildId == buildId &&
+        other.id == id &&
+        other.status == status &&
+        other.insertedAt == insertedAt &&
+        other.applicationId == applicationId;
+  }
+
+  @override
+  // ignore: unnecessary_overrides
+  int get hashCode => super.hashCode;
+}
