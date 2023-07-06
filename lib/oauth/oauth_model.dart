f00a9b2620459073258366870198660894346e2f
jonas-martinez <cbobbxta.fr@gmail.com>
2023-07-06 17:41:58 +0200

    Use params for clientId, redirectUrl and scopes

--- lib/oauth/oauth_model.dart
+++ lib/oauth/oauth_model.dart
@@ -8,17 +8,23 @@ import 'package:oauth2_client/oauth2_helper.dart';
 class OAuthModel extends ChangeNotifier {
   AccessTokenResponse? accessToken;
 
+  String clientId;
+  String redirectUrl;
+  List<String> scopes;
+
+  OAuthModel(this.clientId, this.redirectUrl, {this.scopes = const []}) : super();
+
   Future<AccessTokenResponse?> authenticate() async {
     Completer<AccessTokenResponse?> completer = Completer();
 
     OAuth2Helper oauth2Helper = OAuth2Helper(
       LenraOAuth2Client(
-        redirectUri: "http://localhost:10000/redirect.html",
+        redirectUri: redirectUrl,
         customUriScheme: "http",
       ),
       grantType: OAuth2Helper.authorizationCode,
-      clientId: 'e1773e26-644f-4083-9941-a5406525819d',
-      scopes: ['email', 'public'],
+      clientId: clientId,
+      scopes: scopes,
     );
 
     accessToken = await oauth2Helper.getToken();
