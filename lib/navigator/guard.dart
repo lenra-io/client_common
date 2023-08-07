4e33c2ba0078f5c50b35295f31c166fb40dbc3c3
jonas-martinez <cbobbxta.fr@gmail.com>
2023-08-07 11:55:39 +0200

    Put back the check dev

--- lib/navigator/guard.dart
+++ lib/navigator/guard.dart
@@ -1,6 +1,8 @@
 import 'dart:async';
 
 import 'package:client_common/api/response_models/app_response.dart';
+import 'package:client_common/api/response_models/user.dart';
+import 'package:client_common/models/auth_model.dart';
 import 'package:client_common/models/user_application_model.dart';
 import 'package:client_common/navigator/common_navigator.dart';
 import 'package:flutter/material.dart';
@@ -13,7 +15,15 @@ class Guard {
 
   Guard({required this.isValid, required this.onInvalid});
 
+  static const List<UserRole> _devOrMore = [UserRole.admin, UserRole.dev];
+
+  static final Guard checkIsDev = Guard(isValid: _isDev, onInvalid: _becomeDev);
   static final Guard checkNotHaveApp = Guard(isValid: _haveApp(false), onInvalid: _toHome);
+
+  static Future<bool> _isDev(BuildContext context) async {
+    AuthModel authModel = context.read<AuthModel>();
+    return authModel.isOneOfRole(_devOrMore);
+  }
 
   static Future<String?> guards(BuildContext context, List<Guard> guards) async {
     for (Guard checker in guards) {
@@ -37,6 +47,10 @@ class Guard {
         return false;
       }
     };
+  }
+
+  static String _becomeDev(context) {
+    return CommonNavigator.validationDevRoute;
   }
 
   static String _toHome(context) {
