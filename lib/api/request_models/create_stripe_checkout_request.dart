e65c6d6f7b4ee853bbbb3700d16585ecda87c961
jonas.martinez <cbobbxta.fr@gmail.com>
2023-09-22 15:28:29 +0200

    Add checkout request

---
+++ lib/api/request_models/create_stripe_checkout_request.dart
@@ -0,0 +1,28 @@
+import 'package:client_common/api/request_models/api_request.dart';
+
+class CreateStripeCheckoutRequest extends ApiRequest {
+  String appId;
+  String plan;
+  String mode;
+  String customer;
+  String successUrl;
+  String cancelUrl;
+
+  CreateStripeCheckoutRequest({
+    required this.appId,
+    required this.plan,
+    required this.mode,
+    required this.customer,
+    required this.successUrl,
+    required this.cancelUrl,
+  });
+
+  Map<String, dynamic> toJson() => {
+        'id': appId,
+        'plan': plan,
+        'mode': mode,
+        'customer': customer,
+        'success_url': successUrl,
+        'cancel_url': cancelUrl,
+      };
+}
--- lib/api/stripe_api.dart
+++ lib/api/stripe_api.dart
@@ -1,4 +1,5 @@
 import 'package:client_common/api/lenra_http_client.dart';
+import 'package:client_common/api/request_models/create_stripe_checkout_request.dart';
 import 'package:client_common/api/request_models/create_stripe_customer_request.dart';
 import 'package:client_common/api/response_models/get_stripe_subscriptions_response.dart';
 
@@ -14,7 +15,7 @@ class StripeApi {
         responseMapper: (json, header) => GetStripeSubscriptionsResponse.fromJson(json),
       );
 
-  static Future<String> createCheckout(CreateStripeCustomerRequest body) => LenraApi.instance.post(
+  static Future<String> createCheckout(CreateStripeCheckoutRequest body) => LenraApi.instance.post(
         "/stripe/customers",
         body: body,
         responseMapper: (json, header) => json as String,
