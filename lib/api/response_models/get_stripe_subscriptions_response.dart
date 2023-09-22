5dc35905df53c8ad4a76df1623f20ea28eee5a84
jonas.martinez <cbobbxta.fr@gmail.com>
2023-09-22 18:27:22 +0200

    fix

--- lib/api/response_models/get_stripe_subscriptions_response.dart
+++ lib/api/response_models/get_stripe_subscriptions_response.dart
@@ -3,5 +3,5 @@ import 'package:client_common/api/response_models/api_response.dart';
 class GetStripeSubscriptionsResponse extends ApiResponse {
   String customerId;
 
-  GetStripeSubscriptionsResponse.fromJson(Map<String, dynamic> json) : customerId = json as String;
+  GetStripeSubscriptionsResponse.fromJson(List<Map<String, dynamic>> json) : customerId = json as String;
 }
