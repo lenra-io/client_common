b49f13f687384793405e04de724664476ba67148
jonas.martinez <cbobbxta.fr@gmail.com>
2023-09-25 15:37:50 +0200

    fix

--- lib/api/request_models/create_stripe_checkout_request.dart
+++ lib/api/request_models/create_stripe_checkout_request.dart
@@ -3,7 +3,6 @@ import 'package:client_common/api/request_models/api_request.dart';
 class CreateStripeCheckoutRequest extends ApiRequest {
   int appId;
   String plan;
-  String mode;
   String customer;
   String successUrl;
   String cancelUrl;
@@ -11,7 +10,6 @@ class CreateStripeCheckoutRequest extends ApiRequest {
   CreateStripeCheckoutRequest({
     required this.appId,
     required this.plan,
-    required this.mode,
     required this.customer,
     required this.successUrl,
     required this.cancelUrl,
@@ -20,7 +18,6 @@ class CreateStripeCheckoutRequest extends ApiRequest {
   Map<String, dynamic> toJson() => {
         'id': appId,
         'plan': plan,
-        'mode': mode,
         'customer': customer,
         'success_url': successUrl,
         'cancel_url': cancelUrl,
