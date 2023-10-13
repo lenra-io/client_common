7e19ff51b911456a46e40aabaa0461eae438f97a
jonas.martinez <cbobbxta.fr@gmail.com>
2023-10-13 15:56:01 +0200

    fix: UserResponse can now be null

--- lib/api/response_models/user_response.dart
+++ lib/api/response_models/user_response.dart
@@ -2,7 +2,7 @@ import 'package:client_common/api/response_models/api_response.dart';
 import 'package:client_common/api/response_models/user.dart';
 
 class UserResponse extends ApiResponse {
-  User user;
+  User? user;
 
-  UserResponse.fromJson(Map<String, dynamic> json, Map<String, String> headers) : user = User.fromJson(json);
+  UserResponse.fromJson(Map<String, dynamic>? json, Map<String, String> headers) : user = json == null ? null : User.fromJson(json);
 }
